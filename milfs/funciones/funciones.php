5b39f5ec7cbfbd9befe9f4c6299e3d3c1eb5c3cc
humano <fredyrivera@qwerty.co>
2015-07-11 09:49:01 -0500

    correcion de nuevas rutinas de conexion en funciones.php

--- milfs/funciones/funciones.php
+++ milfs/funciones/funciones.php
@@ -4481,32 +4481,7 @@ return $respuesta;
 }
 $xajax->registerFunction("formulario_modal");
 
-function Conectarse(){
-	if ( !isset ( $link ) ) {
-		
-	include("includes/datos.php");
-   		if(!isset($db)) {
-   include("milfs/includes/datos.php");
-   		}
 
-   if (!($link=mysql_connect($servidor,$usuario,$password)))
-   {
-      echo "Error conectando a la base de datos.";
-      exit();
-   }
-   if (!mysql_select_db($db,$link))
-   {
-      echo "Error seleccionando la base de datos.";
-      exit();
-   }
-   
-	$_SESSION['path']= $path_instalacion;
-	$_SESSION['path_images_secure']= $path_images_secure;
-	$_SESSION['url']= $url;
-	$_SESSION['upload_size']= $upload_size;
-   return $link;
-   }
-}
 
 function limpia_div($capa){
 $respuesta = new xajaxResponse('utf-8');
