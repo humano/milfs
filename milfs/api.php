88ac5ed60c7db66e8a52e20d9f4a628fcd20f9d6
humano <fredyrivera@gmail.com>
2015-07-11 12:39:07 -0500

    Update api.php
    comentar al linea de debugging

--- milfs/api.php
+++ milfs/api.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-ini_set('display_errors', 'On');
+///ini_set('display_errors', 'On');
 require ('xajax/xajax.inc.php');
 $xajax = new xajax();
 require ('funciones/funciones.php');
