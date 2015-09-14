11efc27d412a66e2f03a89ac567d2d31f608fbea
Kleper <kleper@kleper.net>
2015-09-14 11:33:17 -0500

    Update api.php
    Nuevas funciones para la API

--- milfs/api.php
+++ milfs/api.php
@@ -1,14 +1,19 @@
 <?php
 session_start();
-///ini_set('display_errors', 'On');
+//ini_set('display_errors', 'On');
 require ('xajax/xajax.inc.php');
 $xajax = new xajax();
 require ('funciones/funciones.php');
+require ('funciones/convert.php');
+require ("includes/markdown.php");
 require ("funciones/conex.php");
-
 $datos[id]=$_REQUEST[id];
 $datos[identificador]=$_REQUEST[identificador];
 $datos[dato]=$_REQUEST[dato];
+$datos[inicio]=$_REQUEST[inicio];
+$datos[fin]=$_REQUEST[fin];
+$datos[tipo]=$_REQUEST[tipo];
+
 header('Content-Type: application/json');
 
 echo json($datos);
