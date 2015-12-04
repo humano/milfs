f46f0e45f8d05b1f27e50028400ef40d120428ca
humano <fredyrivera@qwerty.co>
2015-12-04 04:39:04 -0500

    BUG Correcion bug en edicion

--- website/index.php
+++ website/index.php
@@ -627,7 +627,7 @@ p.copyright {
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <?php echo $onload;  ?>
+    <?php echo $onload;  ?> 
 </body>
 <?php } ?>
 </html>
